af702112f22d8084749e8137c929fec51681b6fe
Kathrin Markworth <sunnyandkate@Kates-Air.fritz.box>
2021-09-29 11:29:19 +0100

    new commit

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 let slider = document.querySelector('.slider');
-
 let views = document.querySelector('.views');
 let amount = document.querySelector('.amount');
 let checkbox = document.querySelector('#checkbox');
 
-
 slider.oninput = function(){
-
 	let sliderValue = document.querySelector('#slider').value;
-
 
 	if(sliderValue <= 10){
 		
 		if(checkbox.checked == true){
 			 
-		views.innerHTML = 10 + 'K ';
-		amount.textContent = '$' + '6.00';
-
+			views.innerHTML = 10 + 'K ';
+			amount.textContent = '$' + '6.00';
 	}else{
-		views.innerHTML = 10 + 'K ';
-		amount.textContent = '$' + '8.00';
-	}
+			views.innerHTML = 10 + 'K ';
+			amount.textContent = '$' + '8.00';
+		}
 	}
 	
 	if(sliderValue >10 && sliderValue < 25){
@@ -39,11 +34,8 @@ slider.oninput = function(){
 			views.innerHTML = 100 + 'K';
 			amount.textContent = '$' + '12.00';
 		}else{
-			
 			views.innerHTML = 100 + 'K';
 			amount.textContent = '$' + '16.00';
-			
-			
 		}
 
 	}
@@ -53,9 +45,7 @@ slider.oninput = function(){
 			amount.textContent = '$' + '18.00';
 		}else{
 			views.innerHTML = 500 + 'K';
-			amount.textContent = '$' + '24.00';
-			
-			
+			amount.textContent = '$' + '24.00';	
 		}
 
 }
@@ -65,14 +55,10 @@ slider.oninput = function(){
 			amount.textContent = '$' + '27.00';
 		}else{
 			views.innerHTML = 1000 + 'M';
-			amount.textContent = '$' + '36.00';
-			
-			
+			amount.textContent = '$' + '36.00';		
 		}
 
 	}
-	
-
 }
 
 
